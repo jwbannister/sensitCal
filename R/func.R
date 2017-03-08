8926bff77b21ac138bf15848b35f200a1b5edb07
John Bannister <jbannister@airsci.com>
2017-03-07 16:06:09 -0800

    changed to poission regression, cleaned code

---
+++ R/func.R
@@ -0,0 +1,23 @@
+
+#' Pull 1 hour sensit particle counddata for all active sensits 
+#' 
+#' @param day Date. Single day for which sensit data should be pulled. 
+#' @return Data frame.
+summarize_sensit_day <- function(day=Sys.Date()-1){
+    query <- paste0("SELECT i.deployment AS sensit, s.datetime, s.sumpc, ", 
+                    "g.name AS group, a.area AS dca, ",
+                    "flags.is_invalid(s.deployment_id, ",
+                    "s.datetime - '01:00:00'::interval, ",
+                    "s.datetime + '01:00:00'::interval) AS invalid ",
+                    "FROM sensit.sensit_1hour s ",
+                    "LEFT JOIN instruments.deployments i ",
+                    "ON s.deployment_id=i.deployment_id ", 
+                    "LEFT JOIN info.deployment_groups ig ",
+                    "ON s.deployment_id = ig.deployment_id ",
+                    "LEFT JOIN info.groups g ON ig.group_id = g.group_id ",
+                    "LEFT JOIN instruments.areas a ON i.area_id = a.area_id ",
+                    "WHERE (s.datetime -'1 second'::interval)::date='", 
+                    day, "';") 
+    df1 <- query_owens(query) 
+    df1
+}
--- scripts/load.R
+++ scripts/load.R
@@ -1,3 +1,4 @@
+load_all()
 load_all("~/code/owensData")
 library(tidyverse)
 library(ggplot2)
@@ -5,119 +6,74 @@ library(lubridate)
 
 query1 <- "SELECT * FROM sandcatch.sensit_pc_records"
 col_record <- query_owens(query1)
-col_record$name <- factor(col_record$name, ordered=T)
 col_record$ratio <- col_record$dwp_mass/col_record$sumpc_total
-col_dates <- col_record %>% group_by(name) %>%
+col_dates <- col_record %>% group_by(group) %>%
     summarize(date=as.Date(max(collection_datetime)))
 
-# remove outlier data - unrealistic mass/pc ratios
-quantile_filter <- 0.95
+# remove outlier data - unrealistic mass/pc ratios (both high and low)
+quantile_filter <- 0.9
 filter_data <- col_record %>% 
-    filter(ratio < quantile(col_record$ratio, probs=quantile_filter))
+    filter(ratio < quantile(col_record$ratio, probs=quantile_filter) &
+           1/ratio < quantile(1/col_record$ratio, probs=quantile_filter))
 
-lm_list <- vector(mode="list", length=length(plots))
-lm_summary <- data.frame(name=c(), coeff=c(), adj.r.2=c(), p=c())
-names(lm_list) <- names(plots)
-for (j in names(lm_list)){
-    tmp <- filter(filter_data, name==j)
-    lm_list[[j]] <- lm(dwp_mass ~ 0 + sumpc_total, tmp)
-    tmp2 <- data.frame(name=j, coeff=summary(lm_list[[j]])$coefficients[ , 1],  
-                       adj.2=summary(lm_list[[j]])$adj.r.squared, 
-                       p=summary(lm_list[[j]])$coefficients[ , 4]) 
-    lm_summary <- rbind(lm_summary, tmp2)
+# filter to masses in range of desired thresholds
+filter_data2 <- filter_data %>%
+    filter((group=='TwB2' & dwp_mass<2) | (group!='TwB2' & dwp_mass<10))
+
+filter_data3 <- filter_data2
+filter_data3[!(filter_data2$group %in% c('SF Studies', 'TwB2')), ]$group <- 
+    'Other Areas'
+filter_data3$group <- factor(filter_data3$group, ordered=T)
+filter_data3$dwp_mass_count <- filter_data3$dwp_mass * 10
+
+lm_list <- vector(mode="list", length=3)
+names(lm_list) <- c('SF Studies', 'TwB2', 'Other Areas')
+plots <- lm_list
+lm_summary <- data.frame(group=c(), coeff=c(), p=c())
+clrs <- c('SF Studies'='#e41a1c', 'TwB2'='#377eb8', 'Other Areas'='#4daf4a')
+
+for (i in names(plots)){
+    tmp_data <- filter_data3 %>% filter(group==i)
+    lm_list[[i]] <- glm(dwp_mass_count ~ 0 + sumpc_total, data=tmp_data, 
+                        family=poisson)
+    tmp <- data.frame(group=i, 
+                      coeff=summary(lm_list[[i]])$coefficients[ , 1],  
+                      p=summary(lm_list[[i]])$coefficients[ , 4]) 
+    lm_summary <- rbind(lm_summary, tmp)
+    plots[[i]] <-  tmp_data %>%
+        ggplot(aes(x=sumpc_total, y=dwp_mass)) +
+        geom_point(aes(color=group)) +
+        scale_color_manual(values=clrs) +
+        geom_smooth(method="lm", formula = y ~ 0 + x, se=F) +
+        theme(legend.position='none') +
+        ggtitle(names(plots[i]))
 }
-
-new_data <- data.frame(sumpc_total=seq(1, 500, 1))
-predict_list <- vector(mode="list", length=length(lm_list))
-predict_summary <- data.frame(name=c(), sumpc_total=c(), lwr=c())
-names(predict_list) <- names(lm_list)
-thresholds <- c('Brine'=5, 'Channel Area'=5, 'SF Studies'=5, 'T1A-1'=5, 
-                'TwB2'=1)
-for (k in names(predict_list)){
-    thresh <- thresholds[[k]]
-    tmp <- as.data.frame(predict(lm_list[[k]], new_data, interval="prediction", level=0.9))
-    predict_list[[k]] <- cbind(new_data, tmp)
-    tmp2 <- data.frame(name=k, 
-                       threshold=thresh,
-                       sumpc_total=min(filter(predict_list[[k]], lwr>=thresh)$sumpc_total), 
-                       lwr=min(filter(predict_list [[k]], lwr>=thresh)$lwr))
-    predict_summary <- rbind(predict_summary, tmp2)
-
-}
-
-plots <- vector(mode="list", length=length(unique(col_record$name)))
-names(plots) <- levels(col_record$name)
-clrs <- c('#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00')
-full_q_cutoff <- 0.5
-name_q_cutoff <- 0.75
-full_plot <- filter_data %>%
-    filter(sumpc_total<quantile(.$sumpc_total, probs=full_q_cutoff)) %>%
-    ggplot(aes(x=sumpc_total, y=dwp_mass)) +
-    geom_point(aes(color=name)) +
-    theme(legend.position="none", 
-          legend.title=element_blank()) +
-    scale_color_manual(values=clrs) +
-    ggtitle(paste0("All Areas (bottom ", full_q_cutoff*100, "% sumpc_total)"))
-for (i in 1:length(plots)){
-    r2 <- round(lm_summary[lm_summary$name==names(plots)[i], ]$adj.2, 2)
-    th <- predict_summary[predict_summary$name==names(plots)[i], ]$threshold
-    pc <- predict_summary[predict_summary$name==names(plots)[i], ]$sumpc_total
-    cd <- col_dates[col_dates$name==names(plots)[i], ]$date
-    plot_data <- filter_data %>%
-        filter(sumpc_total<quantile(.$sumpc_total, probs=name_q_cutoff)) %>%
-        filter(name==names(plots)[i])
-    x_ann <- max(plot_data$sumpc_total)
-    y_ann <- max(plot_data$dwp_mass)
-    plots[[i]] <-  plot_data %>%
-    ggplot(aes(x=sumpc_total, y=dwp_mass)) +
-    geom_point(color=clrs[i]) +
-    geom_smooth(method="lm", formula = y ~ 0 + x, se=F) +
-    annotate('text', x=x_ann*.5, y=y_ann*.99, size=3, 
-             label=paste0("Most Recent Collection ", cd)) +
-    annotate('text', x=x_ann*.5, y=y_ann*.93, size=3, 
-             label=paste0("R^{2}==", r2), parse=T) +
-    annotate('text', x=x_ann*.5, y=y_ann*.86, size=3, 
-             label=paste0("Mass Threshold = ", th, " g/day")) +
-    annotate('text', x=x_ann*.5, y=y_ann*.79, size=3, 
-             label=paste0("Predicted PC = ", pc)) +
-    ggtitle(paste0(names(plots[i]), " (bottom ", name_q_cutoff*100, 
-                   "% sumpc_total)")) 
-}
-
 fl <- tempfile()
-pdf(file=fl, width=8, height=10.5) 
-gridExtra::grid.arrange(full_plot, plots[[1]], plots[[2]], 
-                        plots[[3]], plots[[4]], plots[[5]], 
-                        nrow=3)
+png(file=fl, width=8, height=8, units="in", res=300) 
+gridExtra::grid.arrange(plots[[1]], plots[[2]], plots[[3]], 
+                        nrow=2)
 dev.off()
 
-doi <- Sys.Date() - 1
-site_areas <- distinct(select(col_record, sensit, name))
-query2 <- paste0("SELECT i.deployment AS sensit, s.datetime, s.sumpc, ", 
-                 "flags.is_invalid(s.deployment_id, ",
-                 "s.datetime - '01:00:00'::interval, ",
-                 "s.datetime + '01:00:00'::interval) AS invalid ",
-                 "FROM sensit.sensit_1hour s ",
-                 "JOIN instruments.deployments i ", 
-                 "ON s.deployment_id=i.deployment_id ", 
-                 "WHERE (s.datetime -'1 second'::interval)::date='", 
-                 doi, "';") 
-sensits_yesterday <- query_owens(query2) %>% 
-    left_join(site_areas, by="sensit")
-sensits_yesterday <- filter(sensits_yesterday, !invalid)
-sensits_yesterday$date <- 
-    as.Date(substring(sensits_yesterday$datetime %m-% seconds(1), 1, 10))
-# ADD IN FUNCTION TO MAKE SURE ALL SENSITS ARE ASSINGED TO AREA (NAME)
-day_pc <- sensits_yesterday %>% group_by(sensit, date, name) %>%
-    summarize(sumpc_total=sum(sumpc)) %>%
-    # TAKE THIS OUT ONCE AREAS ARE PROPERLY ASSIGNED
-    filter(!is.na(name))
-day_pc$flag <- rep(NA, nrow(day_pc))
-for (i in 1:nrow(day_pc)){
-    day_pc$flag[i] <- 
-        ifelse(day_pc[i, ]$sumpc_total >=
-             predict_summary[predict_summary$name==day_pc[i, ]$name, ]$sumpc_total,
-             T, F)
+yesterday_hourly <- summarize_sensit_day()
+yesterday_hourly <- filter(yesterday_hourly, !invalid)
+yesterday_hourly$date <- 
+    as.Date(substring(yesterday_hourly$datetime %m-% seconds(1), 1, 10))
+sensits_yesterday <- yesterday_hourly %>% group_by(sensit, date, group) %>%
+    summarize(sumpc_total=sum(sumpc)) %>% ungroup()
+sensits_yesterday[!(sensits_yesterday$group %in% c('SF Studies', 'TwB2')), ]$group <- 
+    'Other Areas'
+sensits_yesterday$group <- factor(sensits_yesterday$group, ordered=T)
+
+mass_threshold <- c('SF Studies'=5, 'TwB2'=1, 'Other Areas'=5)
+predict_df <- c()
+for (k in levels(sensits_yesterday$group)){
+    new_data <- sensits_yesterday %>% filter(group==k)
+    tmp <- as.data.frame(predict(lm_list[[k]], new_data, type="terms", 
+                                 se.fit=T)) 
+    tmp$flag <- ifelse(tmp$lwr>mass_threshold[[k]], TRUE, FALSE)
+    tmp2 <- cbind(new_data, tmp)
+    predict_df <- rbind(predict_df, tmp2)
 }
 
 
+    
